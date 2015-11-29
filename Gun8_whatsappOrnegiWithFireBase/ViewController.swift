233bdeed3232eff17ab55dc3bd98145892ecee99
OMER <OMER@OMER-MacBook-Pro.local>
2015-11-29 15:44:49 +0200

    Login Successfully Created

--- Gun8_whatsappOrnegiWithFireBase/ViewController.swift
+++ Gun8_whatsappOrnegiWithFireBase/ViewController.swift
@@ -8,8 +8,61 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    var ref = Firebase(url: "https://gun8.firebaseio.com/")
+
+    
+    @IBOutlet weak var txtSifre: UITextField!
+    @IBOutlet weak var txtKullaniciAdi: UITextField!
+    @IBAction func btnLogin(sender: AnyObject) {
+       
+        ref.authUser(txtKullaniciAdi.text, password: txtSifre.text,
+            withCompletionBlock: { error, authData in
+                if error != nil {
+                    // There was an error logging in to this account
+                    println(error)
+                    let alert = UIAlertController(title: "Not Login", message: error.description, preferredStyle: UIAlertControllerStyle.Alert)
+                    
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                    
+                } else {
+                    // We are now logged in
+                    let alert = UIAlertController(title: "Logged-in", message: "Login complete", preferredStyle: UIAlertControllerStyle.Alert)
+                    
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+        })
+
+        
+    }
+    @IBAction func btnRegister(sender: AnyObject) {
+        ref.createUser(txtKullaniciAdi.text, password: txtSifre.text,
+            withValueCompletionBlock: { error, result in
+                
+                if error != nil {
+                    // There was an error logging in to this account
+                    println(error)
+                    let alert = UIAlertController(title: "Not Login", message: error.description, preferredStyle: UIAlertControllerStyle.Alert)
+                    
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                    
+                } else {
+                    // We are now logged in
+                    let alert = UIAlertController(title: "Logged-in", message: "Successfully created user account", preferredStyle: UIAlertControllerStyle.Alert)
+                    
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+
+        })
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
